0d14348d2fe57c381c9f7d8a03a76b93f1fec978
kambe.kenta <kenta.k199405@gmail.com>
2020-11-28 21:20:49 +0900

    initialize repository.

---
+++ src/components/VMain.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container
+    fluid
+    >
+        <v-layout
+        justify-center
+        >
+            <v-btn
+            color="primary"
+            outlined
+            @click="$router.push('/game')"
+            >Game Start</v-btn>
+        </v-layout>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from  'vue-property-decorator';
+
+@Component
+export default class VMain extends Vue {
+
+}
+</script>
---
+++ src/components/reversi/VCell.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="cell-wrapper">
+        <div class="cell"></div>
+        <div class="stone"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from  'vue-property-decorator';
+
+@Component
+export default class VCell extends Vue {
+
+}
+</script>
+
+<style scoped>
+.cell-wrapper {
+    position: relative;
+}
+.cell {
+    height: 64px;
+    width: 64px;
+    background-color: darkcyan;
+    border: 2px solid gray;
+}
+.stone {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: 60px;
+    width: 60px;
+    border-radius: 50%; 
+}
+.white-stone {
+    background-color: white;
+}
+.black-stone {
+    background-color: black;
+}
+
+</style>
